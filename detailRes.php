cdddfe3c383b0ddd1272726049072634c29f31cd
0Hub0 <hubertmgb@gmail.com>
2016-10-14 12:16:10 +0200

    Add files via upload

---
+++ detailRes.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>
+	Detail des Reservations
+	</title>
+	<LINK rel=stylesheet type "css" href="style.css">
+	
+</head>
+
+<body>
+	<h1>Detail des Reservations</h1>
+	
+	
+	<br/>
+	
+	
