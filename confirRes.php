c07f31bf9040b33a10988a90e2db2aa9db02bd59
0Hub0 <hubertmgb@gmail.com>
2016-10-20 22:08:18 +0200

    Add files via upload

---
+++ confirRes.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Confirmation des reservations</title>
+		<LINK rel=stylesheet type "css" href="style.css">
+	</head>
+	<body>
+		<h1>Confirmation des réservations</h1>
+		<div>
+			Votre demande a bien ete enregistree.<br/>
+			Merci de bien vouloir verser la somme de 45 euros sur le compte 000-000000-00
+			<br/><br/>
+			<button type="button" onclick="javascript:location.href='http://localhost/Reservation/main.php'">Retour à la page dd'accueil</button>
+		</div>
+	</body>
+</html>
