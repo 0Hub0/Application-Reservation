de5b7de53f4edcd68a04cd2d9df6a5889aad051e
0Hub0 <hubertmgb@gmail.com>
2016-10-20 22:06:23 +0200

    Update main.php

--- main.php
+++ main.php
@@ -5,50 +5,98 @@
 	</title>
 	<LINK rel=stylesheet type "css" href="style.css">
 	
-	
-	
 </head>
 
 <body>
 	<h1>Reservation</h1>
+			
+			
+			<script>
+			
+			function verif()
+			{
+				<?php
+					$bool=false;
+					'if ($bool==false)
+					{
+						return false
+					}						'
+					
+				?>;
+			}
+			/*function verifNbrP(nbrP)
+			{
+				var Pl=(nbrP.value);tion
+				
+				if (is_int(Pl))
+				{
+					
+					return true;
+				}
+				else 
+				{
+					return false;
+				}
+
+				
+			}*/
+			</script>
+
+			
+			
+	
+	
 	<div>
 	Le prix de la place est de 10 euros jusqu'&agrave 12 ans et ensuite de 15 euros.<br/>
 	Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageurs.
 	<br/><br/>
 	
-	
-	
-	
-	<table>
-		<form method="POST">
+		<form method="POST" ACTION="detailRes.php" onsubmit="return verif()" >
+			<table>
 			<tr>
 				<td>Destination :</td>
-				<td><input type ="text" name="dest[]"></td>
+				<td><input type ="text" name="dest[]" ></td>
 			</tr>
 			<tr>
 				<td>Nombre de places: </td>
-				<td><input type="text" name="nbrP[]"><br/></td>
+				<td><input type="text" name="nbrP[]"  ><br/></td>
 			</tr>
 			<tr>
 				<td>Assurance annulation:</td>
 				<td><input type ="checkbox" name ="insur"><br/></td>
 			</tr>
-	</table>
+		
+			</table>
 		
 		<br/>
-		<input type="submit" name="envoyer" value="Etape Suivante" onclick="alert($suiv)/>
-		<input type="submit" name="retour" value="Annuler la reservation"/>
-	</form>
+		
+		
+		<input type="submit" name="next" value="Etape Suivante" />
+		
+		
+		
+		<input type="reset" name="cancel" value="Annuler la reservation" />
+		</form>
+		
+		
+		
+		
 		
 	</div>
 	
 	
 	
 	
-	
 	<?php 
+
+	
+	
+	
+	
+	
+	
 	//!!!faire un pop-up!!!!
-	/* A Modifier
+	/*A Modifier
 	$res= href="index2.html";
 	$suiv= "<button type=\"button\" onclick=\"alert($res)\" >Test</button>";
 	
