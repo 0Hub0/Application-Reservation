6385934a72a781c18d5575d1276049dcf2a9af9e
0Hub0 <hubertmgb@gmail.com>
2016-10-28 00:55:29 +0200

    Add files via upload

---
+++ page1.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>
+	Reservation
+	</title>
+	<LINK rel=stylesheet type "css" href="style.css">
+	
+</head>
+
+<body>
+	<h1>Reservation</h1>
+	
+	
+	
+	<div>
+	Le prix de la place est de 10 euros jusqu'à 12 ans et ensuite de 15 euros.<br/>
+	Le prix de l'assurance annulation est de 20 euros quel que soit le nombre de voyageurs.
+	<br/><br/>
+	
+		<form method="POST" ACTION="newController.php" >
+		<input type='hidden' name='view1' value=1>	
+			<table>
+				<tr>
+					<td>Destination :</td>
+					<td><input type ="text" name="dest[]" maxlength== "30"></td>
+				</tr>
+				<tr>
+					<td>Nombre de places: </td>
+					<td><input type="text" name="nbrP[]" > <br/> </td>
+				</tr>
+				<tr>
+					<td>Assurance annulation:</td>
+					<td><input type ="checkbox" name ="assur"><br/></td>
+				</tr>
+			</table>
+			<br/>
+		
+			<input type="submit" name="next" value="Etape Suivante" />
+			<input type="reset" name="cancel" value="Annuler la reservation" />
+		</form>
+	</div>
+	
+	
+	
+	
+	
+
+
+
+</body>
+
+</html>
