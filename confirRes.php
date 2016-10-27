2fc0b22bc8e51d85e6bcc7b862b7858631e34da6
0Hub0 <hubertmgb@gmail.com>
2016-10-28 00:50:53 +0200

    Update confirRes.php

--- confirRes.php
+++ confirRes.php
@@ -1,15 +1,20 @@
 <html>
 	<head>
-		<title>Confirmation des reservations</title>
+		<meta charset="utf-8" />
+		<title>Reservation</title>
 		<LINK rel=stylesheet type "css" href="style.css">
 	</head>
 	<body>
 		<h1>Confirmation des réservations</h1>
 		<div>
-			Votre demande a bien ete enregistree.<br/>
-			Merci de bien vouloir verser la somme de 45 euros sur le compte 000-000000-00
+		<form method='POST' action='newController.php'>
+		<input type='hidden' name='view4' value=4>
+			Votre demande a bien ete enregistrée.<br/>
+			Merci de bien vouloir verser la somme de <?php echo $price;?> euros sur le compte 000-000000-00
 			<br/><br/>
-			<button type="button" onclick="javascript:location.href='http://localhost/Reservation/main.php'">Retour à la page dd'accueil</button>
+			<input type='submit' value ="Retour à la page d'acceuil">
+			
+		</form>
 		</div>
 	</body>
 </html>
