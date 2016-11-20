44b0c5cbd7a42cbcf753c59d52d22414cf23be4f
0Hub0 <hubertmgb@gmail.com>
2016-11-20 18:28:19 +0100

    Update page2.php

--- page2.php
+++ page2.php
@@ -12,39 +12,40 @@
 	<h1>Detail des Reservations</h1> <br/>
 	<div>
 		<form method="POST" ACTION="newController.php" >
-		<input type='hidden' name='view2' value=2>
+		<input type='hidden' name='view2' value=2>	
 		<?php
-			for ($i=0;$i<$_SESSION["nbrP"];$i++)
+			for ($i=0;$i<$_SESSION['nbrP'];$i++)
 			{
 				$j=$i+1;
 				echo "
-					<fieldset>
-						<legend><b>InfoVoya".$j."</b></legend>
+					
+						
 							<table>
 								<tr>
 									<td>Nom:</td>
 									<td><input type='text' name='nom[]'></td>
 								</tr>
-								
+							
 								<tr>
 									<td>Age:</td>
 									<td><input type='text' name='age[]'></td>
 								</tr>
 							</table>
-					</fieldset>
+					
 				
 				
 					";
 			}
+			
 		
 		
 		
 		?>
 		
 		<br/>
-		<input type="submit" name="next" value="Etape suivante" />
-		<input type="submit" name="before" value="Retour à la page précédente" />
-		<input type="submit" name="cancel" value="Annuler la réservation" />
+		<input type="submit" name="next2" value="Etape suivante" />
+		<input type="submit" name="before1" value="Retour à la page précédente" />
+		<input type="reset" name="cancel" value="Annuler la réservation" />
 		
 		</form>
 	</div>
