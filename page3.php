03ef23b1e9544aa1784858cb85331d723b6590e7
0Hub0 <hubertmgb@gmail.com>
2016-12-06 23:44:11 +0100

    Update page3.php

--- page3.php
+++ page3.php
@@ -9,34 +9,42 @@
 		<div>
 			
 			<form method='POST' action='newController.php'>
-			<input type='hidden' name='view3' value=3>
+			<input type="hidden" name='view3' value=3>
 			
 				<table>
 					<tr>
 						<td>Destination:</td>
-						<td><?php $reserv->getdest();?></td>
+						<td><?php echo $reserv->getdest();?></td>
 					</tr>
 					<tr>
 						<td>Nombre de places:</td>
-						<td><?php $reserv->getnbrP();?></td>
+						<td><?php echo $reserv->getnbrP();?></td>
 					</tr>
 					
 					
 					<?php
-						for ($i=0;$i<$reserv->getnbrP();$i++)
+						
+						
+						
+						//echo $reserv->getdest();
+						
+					
+						
+						
+						for ($i=0;$i<$_SESSION["nbrP"];$i++)
 						{
-							$j=$i+1;
+							//$j=$i+1;
 							echo "
 									<fieldset>
 											<table>
 												<tr>
 													<td>Nom:</td>
-													<td>".$perso->getname()[$i]."</td>
+													<td>".$pers->getname()[$i]."</td>
 												</tr>
 												
 												<tr>
 													<td>Age:</td>
-													<td>".$perso->getage()[$i]."</td>
+													<td>".$pers->getage()[$i]."</td>
 												</tr>
 											</table>
 									</fieldset>
@@ -47,14 +55,15 @@
 				
 					
 					<tr>
-						<td>Assurance</td>
-						<td><?php $reserv->getassur();?></td>
+						<td>Assurance:</td>
+						<td><?php $_SESSION["assur"];?></td>
 					</tr>
+					
 				</table>
 			
 				
 				<input type="submit" name="next3" value="Confirmer">
-				<input type="submit" name="before2" value="Retour à la page précédente" />
+				<input type="submit" name="before3" value="Retour à la page précédente" />
 				<input type="reset" name="cancel" value="Annuler la réservation" />
 				
 			
