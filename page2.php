d52f5200c4a620bc9b360d0253c11189cace4931
0Hub0 <hubertmgb@gmail.com>
2016-12-06 23:45:08 +0100

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,25 +1,15 @@
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>
-	Reservation
-	</title>
-	<LINK rel=stylesheet type "css" href="style.css">
-	
-</head>
-
 <body>
 	<h1>Detail des Reservations</h1> <br/>
 	<div>
 		<form method="POST" ACTION="newController.php" >
-		<input type='hidden' name='view2' value=2>	
+		<input type="hidden" name='view2' value=2>	
+		
 		<?php
-			for ($i=0;$i<<?php$reserv->getnbrP()[0];?>;$i++)
+			for ($i=0;$i<$_POST['nbrP'];$i++)
 			{
-				$j=$i+1;
+				//$j=$i+1;
 				echo "
 					
-						
 							<table>
 								<tr>
 									<td>Nom:</td>
@@ -44,7 +34,7 @@
 		
 		<br/>
 		<input type="submit" name="next2" value="Etape suivante" />
-		<input type="submit" name="before1" value="Retour à la page précédente" />
+		<input type="submit" name="before2" value="Retour à la page précédente" />
 		<input type="reset" name="cancel" value="Annuler la réservation" />
 		
 		</form>
