361f7742ecee5630c4391922d52a7a0c91ab813d
0Hub0 <hubertmgb@gmail.com>
2016-12-08 21:44:14 +0100

    Update page4.php

--- page4.php
+++ page4.php
@@ -9,37 +9,9 @@
 		<div>
 		<form method='POST' action='newController.php'>
 		<input type="hidden" name='view4' value=4>
-			Votre demande a bien ete enregistrée.<br/>
-			<?php
-			$price=0;
-					//Compute the price
-					
-					
-					for($i=0;$i<$reserv->getnbrP();$i++)
-					{
-						if($pers->getage()[$i]<=12)
-						{
-							$price+=10;
-							
-							echo'Agé de moins de 12 ans';
-							
-						}
-						else
-						{
-							$price+=15;
-							echo'agé de plus de 12 ans';
-						}
-					}
-					
-					if($reserv->getassur() == 'OUI')
-					{
-						$price+=20;
-					}
-					
+			Votre demande a bien ete enregistrée.<br/>	
+			Merci de bien vouloir verser la somme de <?php echo $reserv->getprice() ?> euros sur le compte 000-000000-00";
 			
-
-			echo "Merci de bien vouloir verser la somme de ".$price." euros sur le compte 000-000000-00";
-			?>
 			<br/><br/>
 			<input type='submit' value ="Retour à la page d'acceuil">
 			
